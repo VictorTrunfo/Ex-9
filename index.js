f34fe605334b034bb6dc2ae057662bea3fc240c3
VictorTrunfo <v.trunfo@hotmail.com>
2021-06-21 02:35:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 const dados = require('./dados.json')
 
 app.listen(PORT, function () {
