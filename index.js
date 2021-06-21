08b533a1a11b4c83c676f288f69e99ec6bce62e5
VictorT <68356023+VictorTrunfo@users.noreply.github.com>
2021-06-21 01:51:37 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8080
+const dados = require('./dados.json')
+
+app.get('/api/personagem', function(req, res){
+    const personagem = {
+        "nome": dados.nomes[Math.floor(Math.random() * dados.nomes.length)],
+        "Idioma": dados.idioma[Math.floor(Math.random() * dados.idioma.length)],
+        "poder": Math.floor(Math.random() * 69420) + 5001,
+        "idade" : Math.floor(Math.random() * (40 - 17 + 1) + 17),
+        "vivo": Math.random() > 0.5
+    }
+    res.json(personagem)
+})
+
+app.listen(PORT, function(){
+    console.log("Servidor iniciado")
+})
